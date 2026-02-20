f1451ab185b06f9c0f50da96d28d12b2514c68c4
Wi-Fighter2006 <piyushchudhary97@gmail.com>
2026-02-20 11:17:20 +0530

    Initialize Serial Communication

--- src/main.cpp
+++ src/main.cpp
@@ -5,20 +5,16 @@
  * @brief Embedded Light Intensity Monitoring using LDR
  * @author Piyush Choudhary
  * @date 2026-02-20
- *
- * @details
- * Reads analog brightness data from an LDR sensor
- * connected to LDR_PIN and prints light intensity values
- * to the Serial Monitor.
  */
 
-// STEP 1: Define LDR analog pin
 #define LDR_PIN A0
 
-// Variable to store LDR reading
 int ldrValue = 0;
 
 void setup() {
+
+    // STEP 2: Initialize Serial communication
+    Serial.begin(9600);
 }
 
 void loop() {
